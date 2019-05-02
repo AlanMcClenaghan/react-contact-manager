e56f41b0914e6b44fd703f3cd9123a134b4db450
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-05-02 11:12:48 +0100

    Class component added.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Contact from "./components/Contact";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>App Component</h1>
+      <Contact />
     </div>
   );
 }
