a1f76304cb2ade59c0d8aef0752e7e52625c6c12
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-05-03 12:13:47 +0100

    Events and Changing State

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,17 +9,31 @@ class Contact extends Component {
   //   phone: PropTypes.string.isRequired
   // };
 
+  state = {
+    showContactInfo: false
+  };
+
+  onShowClick = () => {
+    this.setState({ showContactInfo: !this.state.showContactInfo });
+  };
+
   render() {
     // Destructing this.props
     // const { name, email, phone } = this.props;
     const { name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
     return (
       <div className="card card-body mb-3">
-        <h4>{name}</h4>
-        <ul className="list-group">
-          <li className="list-group-item">Email: {email}</li>
-          <li className="list-group-item">Phone: {phone}</li>
-        </ul>
+        <h4>
+          {name} <i onClick={this.onShowClick} className="fas fa-sort-down" />
+        </h4>
+        {showContactInfo ? (
+          <ul className="list-group">
+            <li className="list-group-item">Email: {email}</li>
+            <li className="list-group-item">Phone: {phone}</li>
+          </ul>
+        ) : null}
       </div>
     );
   }
