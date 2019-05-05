47910bed11de359e18ec8e264b33c1befeb97ef4
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-05-05 13:06:12 +0100

    React Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Contacts from "./components/contacts/Contacts";
 import AddContact from "./components/contacts/AddContact";
 import Header from "./components/layout/Header";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound";
 
 import { Provider } from "./context";
 
@@ -11,13 +15,19 @@ import "./App.css";
 function App() {
   return (
     <Provider>
-      <div className="App">
-        <Header heading="Contact Manager" />
-        <div className="container">
-          <AddContact />
-          <Contacts />
+      <BrowserRouter>
+        <div className="App">
+          <Header heading="Contact Manager" />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Contacts} />
+              <Route exact path="/add" component={AddContact} />
+              <Route exact path="/about" component={About} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </BrowserRouter>
     </Provider>
   );
 }
